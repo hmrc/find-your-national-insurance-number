cd1cc66a7b2258e046b378897c7abdf05d6ffbd1
rambabu-posa <rambabu.posa@digital.hmrc.gov.uk>
2023-12-13 16:27:05 +0000

    ITS-3215 Removed mongo references.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,17 +6,14 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "8.2.0"
-  private val hmrcMongoVersion = "1.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "org.typelevel"           %% "cats-core"                  % "2.7.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test,
   )
 
   val it = Seq.empty
