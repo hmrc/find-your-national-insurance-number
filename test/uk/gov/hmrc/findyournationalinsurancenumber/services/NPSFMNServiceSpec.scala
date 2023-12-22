67f5d24431ee5744ef34c9deb146139fec54a4d9
rishisachania <rishisachania@gmail.com>
2023-12-22 16:00:10 +0000

    Revert "ITS-3225 fixing service test"
    This reverts commit 5d8592d44f845fc53f3b8b3538c4243957b4d241.

--- test/uk/gov/hmrc/findyournationalinsurancenumber/services/NPSFMNServiceSpec.scala
+++ test/uk/gov/hmrc/findyournationalinsurancenumber/services/NPSFMNServiceSpec.scala
@@ -3,52 +3,46 @@
  *
  */
 
-import connectors.DefaultNPSFMNConnector
-import models.CorrelationId
-import org.mockito.ArgumentMatchers.any
-import uk.gov.hmrc.http.client.RequestBuilder
-import org.mockito.Mockito._
-import org.scalatestplus.mockito.MockitoSugar
-import org.scalatestplus.play.PlaySpec
-import play.api.http.Status
-import uk.gov.hmrc.http.client.HttpClientV2
-import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
-import config.AppConfig
-import models.nps.NPSFMNRequest
+package uk.gov.hmrc.findyournationalinsurancenumber.services
 
-import java.util.UUID
+import connectors.NPSFMNConnector
+import models.nps.NPSFMNRequest
+import org.mockito.ArgumentMatchers.{any, eq => eqTo}
+import org.mockito.Mockito.{reset, when}
+import play.api.Application
+import services.NPSFMNService
+import uk.gov.hmrc.findyournationalinsurancenumber.util.BaseSpec
+import uk.gov.hmrc.http.HttpResponse
+import play.api.inject.bind
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class DefaultNPSFMNConnectorSpec extends PlaySpec with MockitoSugar {
+class NPSFMNServiceSpec extends BaseSpec {
 
-  "DefaultNPSFMNConnector" should {
+  private val mockNPSFMNConnector = mock[NPSFMNConnector]
 
-    "sendLetter" in {
+  override implicit lazy val app: Application = localGuiceApplicationBuilder()
+    .overrides(
+      bind[NPSFMNConnector].toInstance(mockNPSFMNConnector)
+    )
+    .build()
 
-      val mockAppConfig = mock[AppConfig]
-      val mockHttpClientV2 = mock[HttpClientV2]
-      val mockRequestBuilder = mock[RequestBuilder]
+  override def beforeEach(): Unit =
+    reset(mockNPSFMNConnector)
 
-      val nino = "testNino"
-      val npsFMNRequest = NPSFMNRequest("test", "test", "01-01-1990", "T16 5KX")
-      val correlationId = CorrelationId(UUID.randomUUID())
-      val connector = new DefaultNPSFMNConnector(mockHttpClientV2, mockAppConfig)
-      val expectedResponse = HttpResponse(Status.OK, "")
+  val npsFMNService = inject[NPSFMNService]
 
-      when(mockAppConfig.npsFMNAPIUrl).thenReturn("http://localhost")
-      when(mockAppConfig.npsFMNAPIToken).thenReturn("testToken")
+  "sendLetter" must {
+    "return 200 response when letter is sent successfully" in {
 
-      when(mockHttpClientV2.post(any())(any())).thenReturn(mockRequestBuilder)
-      when(mockRequestBuilder.withBody(NPSFMNRequest(any(),any(),any(),any()))).thenReturn(mockRequestBuilder)
-      when(mockRequestBuilder.setHeader(any())).thenReturn(mockRequestBuilder)
-      when(mockRequestBuilder.execute[HttpResponse](any(),any())).thenReturn(Future.successful(expectedResponse))
+      when(mockNPSFMNConnector.sendLetter(any(), any())(any(), any(), any()))
+        .thenReturn(Future.successful(HttpResponse(200, "success")))
 
-      val result = connector.sendLetter(nino, npsFMNRequest)(HeaderCarrier(), correlationId, global)
+      npsFMNService.sendLetter("test", NPSFMNRequest("test", "test", "01/01/1990", "T16 5KX"))(implicitly, global).map { result =>
+        result.status mustBe 200
+      }(global)
 
-      result.map { response =>
-        response.status mustBe Status.OK
-      }
     }
   }
 }
