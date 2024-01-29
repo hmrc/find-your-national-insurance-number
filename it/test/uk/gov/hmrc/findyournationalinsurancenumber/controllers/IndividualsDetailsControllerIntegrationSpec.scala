97d042348a13713094ef6fd2d5cbaa76ef82db65
ajchilts <40767309+ajchilts@users.noreply.github.com>
2024-01-29 17:01:00 +0000

    Start basics of controller integration tests

--- it/test/uk/gov/hmrc/findyournationalinsurancenumber/controllers/IndividualsDetailsControllerIntegrationSpec.scala
+++ it/test/uk/gov/hmrc/findyournationalinsurancenumber/controllers/IndividualsDetailsControllerIntegrationSpec.scala
@@ -3,40 +3,40 @@
  *
  */
 
-package uk.gov.hmrc.findyournationalinsurancenumber
+package uk.gov.hmrc.findyournationalinsurancenumber.controllers
 
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.Application
+import play.api.http.Status.OK
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.ws.WSClient
+import play.api.test.Helpers.{await, defaultAwaitTimeout}
 
-class HealthEndpointIntegrationSpec
+class IndividualsDetailsControllerIntegrationSpec
   extends AnyWordSpec
      with Matchers
      with ScalaFutures
      with IntegrationPatience
      with GuiceOneServerPerSuite {
 
+  val nino             = "SOME_NINO"
+  val resolveMerge     = "Y"
   private val wsClient = app.injector.instanceOf[WSClient]
   private val baseUrl  = s"http://localhost:$port"
+  private val url      = s"$baseUrl/find-your-national-insurance-number/individuals/details/NINO/$nino/$resolveMerge"
 
   override def fakeApplication(): Application =
     GuiceApplicationBuilder()
       .configure("metrics.enabled" -> false)
       .build()
 
-  "service health endpoint" should {
+  "individuals details endpoint" should {
     "respond with 200 status" in {
-      val response =
-        wsClient
-          .url(s"$baseUrl/ping/ping")
-          .get()
-          .futureValue
-
-      response.status shouldBe 200
+      val response = await(wsClient.url(url).get())
+      response.status shouldBe OK
     }
   }
 }
