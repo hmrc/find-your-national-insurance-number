6a8e72bba93e8c7091241a3dea4f42f096094845
rambabu-posa <rambabu.posa@digital.hmrc.gov.uk>
2024-06-28 10:47:31 +0100

    ITS-3609 Test coverage to 100%.

---
+++ test/config/AppConfigSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import org.scalatestplus.play.PlaySpec
+import play.api.Configuration
+import org.mockito.Mockito._
+import org.scalatestplus.mockito.MockitoSugar
+
+class AppConfigSpec extends PlaySpec with MockitoSugar {
+
+  "AppConfig" should {
+
+    "return DesApiServiceConfig for individualDetails" in {
+      val mockConfig = mock[Configuration]
+      val appConfig = new AppConfig(mockConfig)
+
+      val configMap = Map(
+        "auth-token" -> "testToken",
+        "environment" -> "testEnvironment",
+        "originator-id" -> "testOriginatorId"
+      )
+      val configuration = Configuration.from(configMap)
+
+      when(mockConfig.get[Configuration]("microservice.services.individual-details")).thenReturn(configuration)
+
+      val result = appConfig.individualDetails
+
+      result.token mustBe "testToken"
+      result.environment mustBe "testEnvironment"
+      result.originatorId mustBe "testOriginatorId"
+    }
+  }
+}
