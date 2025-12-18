25e6d96cbab2320d25816b8c6997e2aa589d1132
MrSleeth <5717589+MrSleeth@users.noreply.github.com>
2025-12-18 13:48:12 +0000

    Update HTTP Verbs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,17 @@ object AppDependencies {
 
   private val bootstrapVersion = "10.5.0"
   private val playVersion = "play-30"
+  private val httpVerbsVersion = "15.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% s"bootstrap-backend-$playVersion" % bootstrapVersion,
+    "uk.gov.hmrc" %% s"http-verbs-$playVersion" % httpVerbsVersion,
     "org.typelevel" %% "cats-core" % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootstrapVersion % Test,
+    "uk.gov.hmrc" %% s"http-verbs-test-$playVersion" % httpVerbsVersion,
     "org.mockito" % "mockito-inline" % "5.2.0" % Test
   )
 
